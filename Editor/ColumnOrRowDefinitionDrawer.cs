92d2b200afb210f7404be6dbce245e29b4640483
Zenvin <zenvin@outlook.de>
2023-05-17 23:39:26 +0200

    - Unify Column Property Drawer and Row Property Drawer

--- Editor/ColumnOrRowDefinitionDrawer.cs
+++ Editor/ColumnOrRowDefinitionDrawer.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 using Zenvin.UI.Layout;
 
 namespace Zenvin.UI {
+	[CustomPropertyDrawer (typeof (ColumnDefinition))]
 	[CustomPropertyDrawer (typeof (RowDefinition))]
-	public class RowDefinitionDrawer : PropertyDrawer {
+	public class ColumnOrRowDefinitionDrawer : PropertyDrawer {
 
 		public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
 			position = EditorGUI.PrefixLabel (position, label);
@@ -14,11 +15,12 @@ namespace Zenvin.UI {
 			position.height = EditorGUIUtility.singleLineHeight;
 			position.y += height * 0.5f;
 
-			var valueProp = property.FindPropertyRelative ("height");
+			var valueProp = property.FindPropertyRelative ("width") ?? property.FindPropertyRelative ("height");
 			var unitProp = property.FindPropertyRelative ("unit");
 
 			FloatField (position, valueProp);
-			position.x += position.width;
+			position.x += position.width + EditorGUIUtility.standardVerticalSpacing;
+			position.width -= EditorGUIUtility.standardVerticalSpacing;
 			EditorGUI.PropertyField (position, unitProp, GUIContent.none);
 		}
 
